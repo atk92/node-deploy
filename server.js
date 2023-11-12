fa7f9af3261d9e8fec4edba2293e5be80a6c8c0d
atk92 <104881747+atk92@users.noreply.github.com>
2023-11-12 11:16:35 -0400

    change form for this remount app

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const homeRoutes = require('./src/routes/HomeRoutes')
-class App {
-    constructor() {
-        this.app = express();
-        this.middlewares();
-        this.routes();
-    }
-    middlewares() {
-        this.app.use(express.json())
-        this.app.use(express.urlencoded({ extended: true }))
-    }
-    routes() {
-        this.app.use("/", homeRoutes)
-    }
-}
-const app = new App();
+
+const app = require('./src/App.js')
 const port = process.env.PORT ? Number(process.env.PORT) : 3333
 app.app.listen(port, () => {
     console.log(`RUNNING IN PORT: ${port}`)
