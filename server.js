0892204bc26cf00175e2efb2aeea9e57814b8eae
atk92 <104881747+atk92@users.noreply.github.com>
2023-11-12 11:22:55 -0400

    correção app.app.listen => app.listen()

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
-const app = require('./src/App.js')
+const app = require('./src/App.js');
 const port = process.env.PORT ? Number(process.env.PORT) : 3333
-app.app.listen(port, () => {
+app.listen(port, () => {
     console.log(`RUNNING IN PORT: ${port}`)
 })
